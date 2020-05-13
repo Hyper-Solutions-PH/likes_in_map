88898f7eb0589b737592ddb546eeeb6f078b6196
garipoff <asafgar@yandex.ru>
2020-05-13 21:32:52 +0300

    Добавьте файлы проекта.

---
+++ Map/Common/LocationDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Map.Common
+{
+    public class LocationDetails
+    {
+        public object Location { get; set; }
+    }
+}
---
+++ Map/Hubs/MapHub.cs
@@ -0,0 +1,40 @@
+﻿using Map.Common;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Map.Hubs
+{
+    public class MapHub : Hub
+    {
+        #region Data Members
+
+        static List<LocationDetails> LastLocations = new List<LocationDetails>();
+        #endregion
+
+        #region SendLocation
+        public async Task Send(object location)
+        {
+            AddLocationInCache(location);
+            await Clients.All.SendAsync("heart", location);
+        }
+        #endregion
+
+        #region LastLocations
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("last_hearts", LastLocations);
+            await base.OnConnectedAsync();
+        }
+        #endregion
+
+        #region AddInCache
+        private void AddLocationInCache(object location)
+        {
+            LastLocations.Add(new LocationDetails { Location = location});
+            if (LastLocations.Count > 1000)
+                LastLocations.RemoveAt(0);
+        }
+        #endregion
+    }
+}
